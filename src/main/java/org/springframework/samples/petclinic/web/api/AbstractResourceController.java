72afd1a032c6abba950b937c40c57a8647727dc7
Nils Hartmann <nils@nilshartmann.net>
2016-09-22 21:48:16 +0200

    Enable CORS

--- src/main/java/org/springframework/samples/petclinic/web/api/AbstractResourceController.java
+++ src/main/java/org/springframework/samples/petclinic/web/api/AbstractResourceController.java
@@ -1,8 +1,10 @@
 package org.springframework.samples.petclinic.web.api;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/api")
+@CrossOrigin
 public class AbstractResourceController {
 
 }
