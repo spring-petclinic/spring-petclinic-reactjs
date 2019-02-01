c35e59348c5d50e96dcd1dabdb2c3cb8ad33a3db
Ihor Dmitriiev <Ihor.Dmitriiev@morningstar.com>
2019-02-01 16:03:15 +0200

    CiUi test

--- ui-tests/src/test/java/org/springframework/samples/petclinic/tests/owners/OwnersPageTest.java
+++ ui-tests/src/test/java/org/springframework/samples/petclinic/tests/owners/OwnersPageTest.java
@@ -1,28 +1,49 @@
 package org.springframework.samples.petclinic.tests.owners;
 
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.Selenide;
-import com.github.database.rider.core.api.dataset.DataSet;
+import com.codeborne.selenide.WebDriverRunner;
 
+import org.junit.Before;
+import org.junit.ClassRule;
 import org.junit.Test;
-import org.springframework.samples.petclinic.tests.LocalUiTest;
-import org.springframework.samples.petclinic.util.LoginUtil;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.springframework.samples.petclinic.util.TestContainerUtil;
+import org.testcontainers.containers.BrowserWebDriverContainer;
+import org.testcontainers.containers.DockerComposeContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
 
-import lombok.extern.slf4j.Slf4j;
+import java.io.File;
+import java.time.Duration;
 
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
 
-@Slf4j
-public class OwnersPageTest extends LocalUiTest {
+public class OwnersPageTest {
+  @ClassRule
+  public static DockerComposeContainer dockerComposeContainer = new DockerComposeContainer(new File("../docker-compose.yml"))
+      .withExposedService("application_1", 8080, Wait.forListeningPort().withStartupTimeout(Duration.ofMinutes(3)))
+      .withExposedService("postgres_1", 5432);
 
-  private final LoginUtil loginUtil = new LoginUtil(homePath(), apiLoginPath());
+  BrowserWebDriverContainer chrome;
+
+  @Before
+  public void setUp() {
+    chrome = new BrowserWebDriverContainer()
+        .withCapabilities(DesiredCapabilities.chrome());
+
+    TestContainerUtil.linkContainersNetworks(dockerComposeContainer, chrome, "application_1");
+    chrome.start();
+
+    Configuration.baseUrl = "http://application:8080";
+    WebDriverRunner.setWebDriver(chrome.getWebDriver());
+  }
 
   @Test
-  public void shouldBePossibleToSearchOwnersByLastName() {
+  public void shouldSearchOwnersByLastname() {
     Selenide.open("/");
     $("#username").val("test");
     $("#password").val("testovich");
@@ -32,17 +53,16 @@ public class OwnersPageTest extends LocalUiTest {
     $(byText("Find Owner")).click();
 
     $$("#owners-table tbody tr").shouldHaveSize(1);
-
     ElementsCollection owners = $$("#owners-table tbody tr:nth-child(1) td");
-    owners.get(0).shouldHave(text("George Franklin"));
-    owners.get(1).shouldHave(text("110 W. Liberty St."));
-    owners.get(2).shouldHave(text("Madison"));
-    owners.get(3).shouldHave(text("6085551023"));
-    owners.get(4).shouldHave(text("Leo"));
+    owners.get(0).shouldHave(Condition.text("George Franklin"));
+    owners.get(1).shouldHave(Condition.text("110 W. Liberty St."));
+    owners.get(2).shouldHave(Condition.text("Madison"));
+    owners.get(3).shouldHave(Condition.text("6085551023"));
+    owners.get(4).shouldHave(Condition.text("Leo"));
   }
 
   @Test
-  public void ownerMayBeCreated() {
+  public void shoudlCreateOwner() {
     Selenide.open("/");
     $("#username").val("test");
     $("#password").val("testovich");
@@ -54,62 +74,14 @@ public class OwnersPageTest extends LocalUiTest {
     $("#lastname-input").val("test-lastname");
     $("#address-input").val("test address");
     $("#city-input").val("test city");
-    $("#telephone-input").val("555543434");
+    $("#telephone-input").val("55554343");
 
     $(byText("Add Owner")).click();
 
-    $(byText("Owner Information")).shouldBe(visible);
-    $$("#owners-information-table tbody").shouldHaveSize(1);
-
-    ElementsCollection newOwners = $$("#owners-information-table tbody tr td");
-    newOwners.get(0).shouldHave(text("test-name test-lastname"));
-    newOwners.get(1).shouldHave(text("test address"));
-    newOwners.get(2).shouldHave(text("test city"));
-    newOwners.get(3).shouldHave(text("555543434"));
+    ElementsCollection newOwner = $$("#owners-information-table tbody tr td");
+    newOwner.get(0).shouldHave(Condition.text("test-name test-lastname"));
+    newOwner.get(1).shouldHave(Condition.text("test address"));
+    newOwner.get(2).shouldHave(Condition.text("test city"));
+    newOwner.get(3).shouldHave(Condition.text("55554343"));
   }
-
-  @Test
-  @DataSet(
-      value = {
-          "datasets/test_user.xml",
-          "datasets/owners/owner-to-edit.xml"
-      },
-      executeScriptsBefore = "datasets/cleanup.sql"
-  )
-  public void ownerMayBeEdited() {
-    /*Selenide.open("/");
-
-    $("#username").val("test");
-    $("#password").val("testovich");
-    $("#login-button").click();
-
-    $(byText("FIND OWNERS")).click();
-
-    $("#owner-last-name-input").val("Dmitriev");
-    $(byText("Find Owner")).click();
-
-    $(linkText("Igor Dmitriev")).click();
-    $(byText("Edit Owner")).click();*/
-
-    loginUtil.fastLogin();
-    Selenide.open("/#/owners/1000/edit");
-
-    $("#firstname-input").val("newfirstname");
-    $("#lastname-input").val("newlastname");
-    $("#address-input").val("new street");
-    $("#city-input").val("new city");
-    $("#telephone-input").val("1111");
-
-    $(byText("Update Owner")).click();
-
-    $(byText("Owner Information")).shouldBe(visible);
-    $$("#owners-information-table tbody").shouldHaveSize(1);
-    ElementsCollection owners = $$("#owners-information-table tbody tr td");
-    owners.get(0).shouldHave(text("newfirstname newlastname"));
-    owners.get(1).shouldHave(text("new street"));
-    owners.get(2).shouldHave(text("new city"));
-    owners.get(3).shouldHave(text("1111"));
-  }
-
-
 }
