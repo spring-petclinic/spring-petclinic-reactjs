e003a1d0a44b6f5ef14eca74318845b7ea12f866
kietzman <kietzman@DESKTOP-87VEK1E>
2023-11-06 15:44:54 +0100

    changed code in PetResource.java

--- src/main/java/org/springframework/samples/petclinic/web/api/PetResource.java
+++ src/main/java/org/springframework/samples/petclinic/web/api/PetResource.java
@@ -51,7 +51,7 @@ public class PetResource extends AbstractResourceController {
 
 	@GetMapping("/pettypes")
 	Object getPetTypes() {
-		return clinicService.findPetTypes();
+		return "Welcome";
 	}
 
 	@PostMapping("/owners/{ownerId}/pets")
